6870e9af26a4ddef0f2fcf73684fb35915fb44d5
Mykolas Juraitis <mykolas.juraitis@gmail.com>
2020-10-03 22:47:56 +0100

    Proof of concept using of DynamixelSDK C++ code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include "dynamixel_sdk/dynamixel_sdk.h"
+
+using namespace std;
+
 int main(int argc, const char *argv[]) {
+  dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler("/dev/ttyUSB0");
+  if (portHandler->openPort()) {
+    cout << "Succeeded to open the port" << endl;
+  }
+  else {
+    cout << "Failed to open the port!" << endl;
+  }
+
   return 0;
 }
