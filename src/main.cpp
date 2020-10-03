f4e9215a3dac7800b5bb7e5f8e322c809c3e4c39
Mykolas Juraitis <mykolas.juraitis@gmail.com>
2020-10-04 00:49:47 +0100

    Add librobotcontrol

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "dynamixel_sdk/dynamixel_sdk.h"
+#include "robotcontrol.h"
 
 using namespace std;
 
 int main(int argc, const char *argv[]) {
+  if (rc_adc_init()) {
+    cout << "Failed to run rc_init_adc()" << endl;
+  } else {
+    cout << "Battery voltage: " << rc_adc_batt() << endl;
+  }
+
   dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler("/dev/ttyUSB0");
   if (portHandler->openPort()) {
     cout << "Succeeded to open the port" << endl;
