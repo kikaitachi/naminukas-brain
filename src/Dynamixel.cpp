40b0db410ac10fbc24837341fab0f8c799ae0975
Mykolas Juraitis <mykolas.juraitis@sg-tech.co.uk>
2020-10-13 21:47:28 +0100

    Add files for Dynamixel control

---
+++ src/Dynamixel.cpp
@@ -0,0 +1,2 @@
+#include "dynamixel_sdk/dynamixel_sdk.h"
+#include "Dynamixel.hpp"
---
+++ src/Dynamixel.hpp
@@ -0,0 +1,9 @@
+#ifndef NAMINUKAS_BRAIN_DYNAMIXEL_H_
+#define NAMINUKAS_BRAIN_DYNAMIXEL_H_
+
+class Dynamixel {
+  public:
+    int id;
+};
+
+#endif  // NAMINUKAS_BRAIN_DYNAMIXEL_H_
