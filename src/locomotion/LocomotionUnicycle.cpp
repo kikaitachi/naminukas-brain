81ff0a173fb85018c094c76120a3ae1cacc8f9bb
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2021-01-30 09:25:57 +0000

    Try half-stationary walking

--- src/locomotion/LocomotionUnicycle.cpp
+++ src/locomotion/LocomotionUnicycle.cpp
@@ -1,9 +1,11 @@
 #include "LocomotionUnicycle.hpp"
 
-#define ANGLE 50
+#define STATIONARY_ANGLE 60
+#define WALKING_ANGLE 3
+#define GLIDE_RATIO 1.5
 
 LocomotionUnicycle::LocomotionUnicycle(hardware::Kinematics& kinematics, IMU& imu)
-    : Locomotion(200), roll_controller(2, 0, 0, 20, -3, 3), kinematics(kinematics), imu(imu) {
+    : Locomotion(100), roll_controller(2, 0, 0, 20, -3, 3), kinematics(kinematics), imu(imu) {
 }
 
 std::string LocomotionUnicycle::name() {
@@ -15,11 +17,11 @@ void LocomotionUnicycle::control_loop() {
   float goal_roll = -37;
   float roll_input = roll_controller.input(roll, goal_roll);
 
-  kinematics.set_joint_position({
-    { hardware::Joint::left_ankle, initial_ankle_angle - ANGLE - roll_input },
+  /*kinematics.set_joint_position({
+    { hardware::Joint::left_ankle, initial_ankle_angle - STATIONARY_ANGLE - roll_input },
     { hardware::Joint::left_wheel, - roll_input }
-  });
-  logger::debug("roll: %f, angle: %f, fitness: %f", roll, ANGLE + roll_input, roll_controller.get_fitness());
+  });*/
+  logger::debug("roll: %f, angle: %f, fitness: %f", roll, STATIONARY_ANGLE + roll_input, roll_controller.get_fitness());
 }
 
 void LocomotionUnicycle::on_start() {
@@ -28,7 +30,7 @@ void LocomotionUnicycle::on_start() {
   kinematics.set_joint_control_mode(hardware::Joint::right_ankle, hardware::JointControlMode::position);
   kinematics.set_joint_control_mode(hardware::Joint::right_wheel, hardware::JointControlMode::position);
   kinematics.set_joint_position({
-    { hardware::Joint::left_ankle, initial_ankle_angle - ANGLE },
+    { hardware::Joint::left_ankle, initial_ankle_angle - STATIONARY_ANGLE },
     { hardware::Joint::right_ankle, initial_ankle_angle },
   });
   roll_controller.reset();
@@ -42,7 +44,16 @@ void LocomotionUnicycle::on_stop() {
 }
 
 void LocomotionUnicycle::up(bool key_down, std::set<std::string>& modifiers) {
-  // TODO: implement
+  std::vector<hardware::JointPosition> curr_pos = kinematics.get_joint_position({
+    hardware::Joint::left_wheel
+  });
+  double angle = modifiers.find("Control") != modifiers.end() ? WALKING_ANGLE : -WALKING_ANGLE;
+  if (curr_pos.size() != 0) {
+    kinematics.set_joint_position({
+      { hardware::Joint::left_ankle, initial_ankle_angle - STATIONARY_ANGLE + angle },
+      { hardware::Joint::left_wheel, curr_pos[0].degrees + angle * GLIDE_RATIO },
+    });
+  }
 }
 
 void LocomotionUnicycle::down(bool key_down, std::set<std::string>& modifiers) {
