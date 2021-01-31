aadec4616edaa406c743d15820041c2d00efad34
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2021-01-31 10:43:45 +0000

    Don't rely on reached_destination function as it is broken

--- src/action/ActionRotate.cpp
+++ src/action/ActionRotate.cpp
@@ -1,25 +1,27 @@
+#include <cmath>
 #include "../Logger.hpp"
 #include "ActionRotate.hpp"
+
+#define POS_THRESHOLD 0.2
 
 ActionRotate::ActionRotate(hardware::Kinematics& kinematics, std::vector<hardware::JointPosition> positions, int how)
     : kinematics(kinematics), positions(positions), how(how) {
 }
 
 void ActionRotate::start() {
-  std::vector<hardware::JointPosition> destination;
   if (how == ABSOLUTE) {
-    destination = positions;
+    goal = positions;
   } else {
-    destination = kinematics.get_joint_position(joints(positions));
-    if (destination.size() == positions.size()) {
+    goal = kinematics.get_joint_position(joints(positions));
+    if (goal.size() == positions.size()) {
       for (int i = 0; i < positions.size(); i++) {
-        destination[i].degrees += positions[i].degrees;
+        goal[i].degrees += positions[i].degrees;
       }
     } else {
       logger::error("Failed to retrieve current joint angles for relative rotation action");
     }
   }
-  kinematics.set_joint_position(destination);
+  kinematics.set_joint_position(goal);
 }
 
 void ActionRotate::abort() {
@@ -27,7 +29,13 @@ void ActionRotate::abort() {
 }
 
 bool ActionRotate::execute() {
-  return kinematics.reached_destination(joints(positions));
+  std::vector<hardware::JointPosition> current = kinematics.get_joint_position(joints(positions));
+  for (int i = 0; i < current.size(); i++) {
+    if (fabs(current[i].degrees - goal[i].degrees) > POS_THRESHOLD) {
+      return false;
+    }
+  }
+  return true;
 }
 
 std::vector<hardware::Joint> ActionRotate::joints(std::vector<hardware::JointPosition> positions) {
