1bfd79574ef8584513d78cf93b82c6c95b5144f1
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2021-02-22 20:25:49 +0000

    First try for 'Hang' locomotion

--- src/locomotion/LocomotionHang.cpp
+++ src/locomotion/LocomotionHang.cpp
@@ -15,12 +15,12 @@ Pose LocomotionHang::control_loop(Pose pose) {
 }
 
 void LocomotionHang::on_start() {
-  kinematics.set_joint_control_mode(hardware::Joint::left_wheel, hardware::JointControlMode::position);
+  kinematics.set_joint_control_mode(hardware::Joint::left_wheel, hardware::JointControlMode::velocity, 2000);
   kinematics.set_joint_control_mode(hardware::Joint::left_ankle, hardware::JointControlMode::position);
   kinematics.set_joint_control_mode(hardware::Joint::right_ankle, hardware::JointControlMode::position);
-  kinematics.set_joint_control_mode(hardware::Joint::right_wheel, hardware::JointControlMode::position);
+  kinematics.set_joint_control_mode(hardware::Joint::right_wheel, hardware::JointControlMode::velocity, 2000);
   kinematics.set_joint_position({
-    { hardware::Joint::left_ankle, initial_ankle_angle },
+    { hardware::Joint::left_ankle, initial_ankle_angle - 90 },
     { hardware::Joint::right_ankle, initial_ankle_angle }
   });
 }
@@ -30,4 +30,40 @@ void LocomotionHang::on_stop() {
   kinematics.set_joint_control_mode(hardware::Joint::left_ankle, hardware::JointControlMode::off);
   kinematics.set_joint_control_mode(hardware::Joint::right_ankle, hardware::JointControlMode::off);
   kinematics.set_joint_control_mode(hardware::Joint::right_wheel, hardware::JointControlMode::off);
+}
+
+void LocomotionHang::halt() {
+  kinematics.set_joint_speed({ { hardware::Joint::left_wheel, 0 }, { hardware::Joint::right_wheel, 0 } });
+}
+
+void LocomotionHang::up(bool key_down, std::set<std::string>& modifiers) {
+  if (key_down) {
+    kinematics.set_joint_speed({ { hardware::Joint::left_wheel, -max_rpm }, { hardware::Joint::right_wheel, max_rpm } });
+  } else {
+    halt();
+  }
+}
+
+void LocomotionHang::down(bool key_down, std::set<std::string>& modifiers) {
+  if (key_down) {
+    kinematics.set_joint_speed({ { hardware::Joint::left_wheel, max_rpm }, { hardware::Joint::right_wheel, -max_rpm } });
+  } else {
+    halt();
+  }
+}
+
+void LocomotionHang::left(bool key_down, std::set<std::string>& modifiers) {
+  if (key_down) {
+    kinematics.set_joint_speed({ { hardware::Joint::left_wheel, max_rpm }, { hardware::Joint::right_wheel, max_rpm } });
+  } else {
+    halt();
+  }
+}
+
+void LocomotionHang::right(bool key_down, std::set<std::string>& modifiers) {
+  if (key_down) {
+    kinematics.set_joint_speed({ { hardware::Joint::left_wheel, -max_rpm }, { hardware::Joint::right_wheel, -max_rpm } });
+  } else {
+    halt();
+  }
 }
--- src/locomotion/LocomotionHang.hpp
+++ src/locomotion/LocomotionHang.hpp
@@ -16,8 +16,14 @@ class LocomotionHang: public Locomotion {
   Pose control_loop(Pose pose);
   void on_start();
   void on_stop();
+  void up(bool key_down, std::set<std::string>& modifiers);
+  void down(bool key_down, std::set<std::string>& modifiers);
+  void left(bool key_down, std::set<std::string>& modifiers);
+  void right(bool key_down, std::set<std::string>& modifiers);
  private:
   hardware::Kinematics& kinematics;
+  const double max_rpm = 5;
+  void halt();
 };
 
 #endif  // SRC_LOCOMOTION_LOCOMOTIONHANG_HPP_
