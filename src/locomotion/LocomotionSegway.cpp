b91656446b6053b64af44099afd28bc7d89bb4fc
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2021-03-04 20:57:32 +0000

    Even more PID tuning

--- src/locomotion/LocomotionSegway.cpp
+++ src/locomotion/LocomotionSegway.cpp
@@ -14,8 +14,8 @@
 
 LocomotionSegway::LocomotionSegway(hardware::Kinematics& kinematics, IMU& imu)
     : Locomotion(CONTROL_LOOP_FREQUENCY), kinematics(kinematics), imu(imu),
-    speed_controller(0.01, 0, 0, 25, -40, 40),
-    pitch_controller(2.5, 0, 0.1, 25, -std::numeric_limits<float>::max(), std::numeric_limits<float>::max()) {
+    speed_controller(0.2, 0, 0, 25, -40, 40),
+    pitch_controller(2.5, 0, 0, 25, -std::numeric_limits<float>::max(), std::numeric_limits<float>::max()) {
 }
 
 std::string LocomotionSegway::name() {
@@ -47,9 +47,12 @@ Pose LocomotionSegway::control_loop(Pose pose) {
 
   float rpm_left = curr_pos[0].rpm;
   float rpm_right = -curr_pos[1].rpm;
-  rpm_avg = LOW_PASS(rpm_avg, (rpm_left + rpm_right) / 2, 0.5);
-  float goal_pitch = -2.49;  // + speed_controller.input(rpm_avg, goal_rpm);
-  pitch = LOW_PASS(pitch, imu.get_pitch(), 0.5);
+  rpm_avg = LOW_PASS(rpm_avg, (rpm_left + rpm_right) / 2, 0.05);
+  //rpm_avg = (rpm_left + rpm_right) / 2;
+  // float goal_pitch = -2.49;
+  float goal_pitch = -2.49 - speed_controller.input(rpm_avg, goal_rpm);
+  // pitch = LOW_PASS(pitch, imu.get_pitch(), 0.5);
+  pitch = imu.get_pitch();
   float input = pitch_controller.input(pitch, goal_pitch);
 
   float new_position_left = curr_pos[0].position + input + left_turn_speed;
