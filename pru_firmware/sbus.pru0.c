0a04f8472288eb122d98926227f275fd46728a3b
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2021-06-21 21:19:35 +0000

    Trying to get full packet read correctly

--- pru_firmware/sbus.pru0.c
+++ pru_firmware/sbus.pru0.c
@@ -7,37 +7,32 @@ volatile register unsigned int __R31;
 
 #define CYCLES_PER_UART_BIT 2000
 
-void reset_iep(void) {
+inline void reset_iep(void) {
   // Set counter to 0
   CT_IEP.TMR_CNT = 0x0;
   // Enable counter
   CT_IEP.TMR_GLB_CFG = 0x11;
 }
 
-int read_iep(void) {
+inline int read_iep(void) {
   // Return counter content
   return CT_IEP.TMR_CNT;
 }
 
-int read_pin() {
+inline int read_pin() {
   return __R31 & (1 << 15) ? 1 : 0;
 }
 
-void delay(uint32_t count) {
-  reset_iep();
-  while (read_iep() < count) {}
-}
-
 uint8_t read_byte() {
-  delay(2100);
+  reset_iep();
   uint8_t result = 0;
   int i;
-  for (i = 0; i < 8; i++) {
+  for (i = 1; i <= 8; i++) {
+    while (read_iep() < CYCLES_PER_UART_BIT * i + CYCLES_PER_UART_BIT / 2) {}
     result = (result << 1) | read_pin();
-    delay(2000);
   }
   // Ignore parity for now
-  //while (read_iep() < CYCLES_PER_UART_BIT * 9) { }
+  while (read_iep() < CYCLES_PER_UART_BIT * 9 + CYCLES_PER_UART_BIT / 2) { }
   // 2 stop bits can be safely ignored
   return result;
 }
@@ -59,18 +54,17 @@ void main(void) {
       reset_iep();
       // Wait for start bit
       while (!read_pin()) {
-        if (read_iep() > 40000) {
+        if (read_iep() > CYCLES_PER_UART_BIT * 20) {
           goto restart;
         }
       }
       sbus_packet[i] = read_byte();
     }
-    //*addr = ((uint32_t)sbus_packet[1]) << 3 | ((uint32_t)sbus_packet[2]) >> 5;
-    /*while (!read_pin()) { }
-    reset_iep();
-    while (read_pin()) { }
-    *addr = read_iep();*/
-    *addr = sbus_packet[0]; // Header 0x0F
-    //*addr = (*addr << 1) | read_pin();
+    //if (sbus_packet[24] == 0) {
+      uint32_t byte1 = sbus_packet[1];
+      uint32_t byte2 = sbus_packet[2];
+      *addr = (byte1 << 3) | (byte2 >> 5);
+    //}
+    *addr = sbus_packet[24]; // Header 0x0F
   }
 }
