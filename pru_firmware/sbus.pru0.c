3c678a169c2071d4c251e08941ae3ad95b4cf0b1
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2021-06-21 20:39:57 +0000

    First successful read of the header

--- pru_firmware/sbus.pru0.c
+++ pru_firmware/sbus.pru0.c
@@ -1,41 +1,43 @@
 #include <stdint.h>
 #include <pru_cfg.h>
-#include "resource_table_empty.h"
-#include "prugpio.h"
 #include <pru_iep.h>
+#include "resource_table_empty.h"
 
 volatile register unsigned int __R31;
 
 #define CYCLES_PER_UART_BIT 2000
 
-inline void reset_iep(void) {
+void reset_iep(void) {
   // Set counter to 0
   CT_IEP.TMR_CNT = 0x0;
   // Enable counter
   CT_IEP.TMR_GLB_CFG = 0x11;
 }
 
-inline int read_iep(void) {
+int read_iep(void) {
   // Return counter content
   return CT_IEP.TMR_CNT;
 }
 
-inline int read_pin() {
-  uint32_t bits = __R31;
-  uint32_t mask = 0x8000;
-  return bits & mask ? 0 : 1;
+int read_pin() {
+  return __R31 & (1 << 15) ? 1 : 0;
 }
 
-inline uint8_t read_byte() {
+void delay(uint32_t count) {
   reset_iep();
+  while (read_iep() < count) {}
+}
+
+uint8_t read_byte() {
+  delay(2100);
   uint8_t result = 0;
   int i;
   for (i = 0; i < 8; i++) {
-    while (read_iep() < CYCLES_PER_UART_BIT * (i + 1)) { }
     result = (result << 1) | read_pin();
+    delay(2000);
   }
   // Ignore parity for now
-  while (read_iep() < CYCLES_PER_UART_BIT * 8) { }
+  //while (read_iep() < CYCLES_PER_UART_BIT * 9) { }
   // 2 stop bits can be safely ignored
   return result;
 }
@@ -45,6 +47,7 @@ void main(void) {
   CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
   // Use then same address as librobot control for quadrature encoder 4
+  // in order not to mess up other PRU used for servo motor control
   uint32_t *addr = (uint32_t*)(0x10000 + 16 * 4);
   *addr = 0;
 
@@ -55,17 +58,19 @@ void main(void) {
     for (i = 0; i < 25; i++) {
       reset_iep();
       // Wait for start bit
-      while (!read_pin()) { }
-      if (i != 0 && read_iep() > CYCLES_PER_UART_BIT * 5) {
-        // If is not the first byte and start bit took longer than 5 clock cycles 
-        goto restart;
+      while (!read_pin()) {
+        if (read_iep() > 40000) {
+          goto restart;
+        }
       }
       sbus_packet[i] = read_byte();
     }
-    uint8_t header = read_byte();  // header
-    uint32_t byte1 = read_byte();
-    uint32_t byte2 = read_byte();
-    //*addr = byte1 << 3 | byte2 >> 5;
+    //*addr = ((uint32_t)sbus_packet[1]) << 3 | ((uint32_t)sbus_packet[2]) >> 5;
+    /*while (!read_pin()) { }
+    reset_iep();
+    while (read_pin()) { }
+    *addr = read_iep();*/
     *addr = sbus_packet[0]; // Header 0x0F
+    //*addr = (*addr << 1) | read_pin();
   }
 }
